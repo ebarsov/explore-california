e1d61f18ec9c06e201fc22ba488fe490d323ba56
ebarsov <109013764+ebarsov@users.noreply.github.com>
2022-10-17 17:39:39 -0700

    Add files via upload

---
+++ it162/template.php
@@ -0,0 +1,7 @@
+<?php include 'includes/header.php';?>
+        
+<h2 class="pageID">Template</h2>
+
+<p>Use this as a model for creating additional pages.</p>
+ 
+<?php include 'includes/footer.php';?>
